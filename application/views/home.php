ef45167e074aee3d166d8e0a94ffc0bc571156b1
danpvsocson <ilovefamilyhk9@gmail.com>
2022-05-06 16:41:47 +0700

    up

---
+++ application/views/home.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dữ Liệu Sim</title>
+    <!-- * Boostrap  --->
+    <link rel="stylesheet" href="./assets/css/bootstrap-5.1.3-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./assets/css/aos-animasiton-scroll/dist/aos.css">
+    <link rel="stylesheet" href="./assets/font/fontawesome-free-5.15.4-web/css/all.min.css">
+    <link rel="stylesheet" href="./assets/css/swiper-slide.css">
+    <!--* JQuery -->
+    <script type="text/javascript" src="./assets/js/main.js"></script>
+    <script src="./assets/js/jquery-3.6.0.min.js"></script>
+</head>
+<body>
+    
+    <script type="text/javascript" src="./assets/css/bootstrap-5.1.3-dist/js/bootstrap.min.js"></script>
+    <script src="./assets/css/aos-animasiton-scroll/dist/aos.js"></script>
+    <script>
+        AOS.init({
+            once: true
+        });
+    </script>
+</body>
+</html>
