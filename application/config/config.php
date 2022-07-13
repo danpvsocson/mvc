419ac7d8feacb1948db642b21c6d0438c32ca209
danpvsocson <ilovefamilyhk9@gmail.com>
2022-07-13 20:17:34 +0700

    3

--- application/config/config.php
+++ application/config/config.php
@@ -26,7 +26,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 /* Nhập tên miền ở đây */
 $config['base_url'] = 'https://localhost/admin';
-// $config['base_url'] = 'http://danpv.ga';
+// $config['base_url'] = 'http://yasua.ga';
 
 /*
 |--------------------------------------------------------------------------
