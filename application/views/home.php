e617fcc4a8d708cecff01f3ff661b41240df883a
danpv.socsson <ilovefamilyhk9@gmail.com>
2023-08-10 18:59:27 +0700

    update

--- application/views/home.php
+++ application/views/home.php
@@ -5,8 +5,46 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Trang Chu</title>
+    <link rel="stylesheet" href="assets/css/bootstrap-5.1.3-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/aos-animasiton-scroll/dist/aos.css">
+    <link rel="stylesheet" href="assets/font/fontawesome-free-5.15.4-web/css/all.min.css">
+    <link rel="stylesheet" href="assets/css/swiper-slide.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/responsive.css">
+    <!--* JQuery -->
+    <script type="text/javascript" src="assets/js/main.js"></script>
+    <script src="assets/js/jquery-3.6.0.min.js"></script>
+    <script src="assets/js/jquery-upload.js"></script>
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+            background-image: url('https://i.pinimg.com/originals/2d/a8/88/2da888655c16c67d8dd1dbedd95cfd4c.jpg'); /* Replace with the actual path to your image */
+            background-size: cover;
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+        }
+
+        .content {
+            padding: 20px;
+            color: white;
+        }
+    </style>
 </head>
-<body>
-    Xin chao
+<body class="home">
+    <div class=" d-flex justify-content-evenly align-items-center" style="margin-top:100px">
+        <div><a href="https://danpv-school.netlify.app/" class="btn btn-info">Page School</a></div>
+        <div><a href="https://danpv.o.edu.vn/admin" class="btn btn-info">Page Admin</a></div>
+    </div>
+    <div class=" d-flex justify-content-center mt-5">
+        <p>Bạn Hãy Chọn 1 Để Xem Thêm</p>
+    </div>
+    <script type="text/javascript" src="assets/css/bootstrap-5.1.3-dist/js/bootstrap.min.js"></script>
+    <script src="assets/css/aos-animasiton-scroll/dist/aos.js"></script>
+    <script>
+        AOS.init({
+            once: true
+        });
+    </script>
 </body>
 </html>
